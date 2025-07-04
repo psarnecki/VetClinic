878037dd97d8b8a4e64a014e8fbb895bcf1ad640
psarnecki <piotr.sarnecki03@gmail.com>
2025-07-05 00:10:03 +0200

    Initial project structure

---
+++ VetClinicManager.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace VetClinicManager.Tests.Unit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
