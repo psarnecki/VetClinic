27feec1fdc3db2e35da1151b84c80badc6ddbd77
psarnecki <piotr.sarnecki03@gmail.com>
2025-07-07 12:21:26 +0200

    Fix: Correct user registration for custom User model

--- VetClinicManager/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ VetClinicManager/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -76,6 +76,16 @@ namespace VetClinicManager.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
+            [StringLength(80)]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [StringLength(80)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+            
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -114,6 +124,9 @@ namespace VetClinicManager.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
+                
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
