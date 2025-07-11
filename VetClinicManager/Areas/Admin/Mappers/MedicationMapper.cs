f4ae8dc64dccfdb4f61d4a38723e328fc903a46d
psarnecki <piotr.sarnecki03@gmail.com>
2025-07-11 23:30:05 +0200

    feat(Admin): Implement Mapperly for medication entities

---
+++ VetClinicManager/Areas/Admin/Mappers/MedicationMapper.cs
@@ -0,0 +1,25 @@
+﻿using Riok.Mapperly.Abstractions;
+using VetClinicManager.Areas.Admin.DTOs.Medications;
+using VetClinicManager.Models;
+
+namespace VetClinicManager.Areas.Admin.Mappers;
+
+[Mapper]
+public partial class MedicationMapper
+{
+    // Mapping from Medication to MedicationListDto - single entity object (Index)
+    public partial MedicationListDto ToMedicationListDto(Medication medication);
+
+    // Mapping from Medication to MedicationEditDto (Edit GET)
+    public partial MedicationEditDto ToMedicationEditDto(Medication medication);
+
+    // Mapping from Medication to MedicationDeleteDto (Delete GET)
+    public partial MedicationDeleteDto ToMedicationDeleteDto(Medication medication);
+
+    // Mapping from MedicationCreateDto to Medication (Create POST)
+    public partial Medication ToMedication(MedicationCreateDto createDto);
+
+    // Mapping from MedicationEditDto to existing Medication (Edit POST)
+    [MapperIgnoreSource(nameof(MedicationEditDto.Id))]
+    public partial void UpdateMedicationFromDto(MedicationEditDto editDto, Medication medication);
+}
