934695e385419c510d7093224d1bb3fee4d3c041
psarnecki <piotr.sarnecki03@gmail.com>
2025-07-28 23:40:48 +0200

    Add and configure DTOs for VisitUpdate and Prescription

---
+++ VetClinicManager/DTOs/VisitUpdates/VisitUpdateCreateDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using VetClinicManager.DTOs.Prescriptions;
+
+namespace VetClinicManager.DTOs.VisitUpdates;
+
+public class VisitUpdateCreateDto
+{
+    [Required]
+    public int VisitId { get; set; }
+    
+    public string? VisitTitle { get; set; }
+    
+    public string? AnimalName { get; set; }
+    
+    [Display(Name = "Clinical Notes")]
+    public string? Notes { get; set; }
+    
+    [Display(Name = "Attachment (Image)")]
+    public IFormFile? ImageFile { get; set; }
+    
+    public SelectList? Medications { get; set; }
+    
+    public List<PrescriptionCreateDto> Prescriptions { get; set; } = new();
+}
