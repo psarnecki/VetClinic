1686062a2c22e725c92f33127936db33e03c6c95
psarnecki <piotr.sarnecki03@gmail.com>
2026-03-04 23:37:42 +0100

    Add and configure DTOs for Dashboard

---
+++ VetClinicManager/DTOs/Home/DashboardDto.cs
@@ -0,0 +1,25 @@
+﻿using VetClinicManager.DTOs.Shared;
+
+namespace VetClinicManager.DTOs.Home;
+
+public class DashboardDto
+{
+    // Common stats
+    public int TotalAnimals { get; set; }
+    public int TotalVisits { get; set; }
+    
+    // For Admin/Receptionist/Vet
+    public int TodayVisits { get; set; }
+    public int UpcomingVisits { get; set; }
+    public int TotalClients { get; set; }
+    public int TotalMedications { get; set; }
+    
+    // For Client
+    public int MyAnimals { get; set; }
+    public int MyUpcomingVisits { get; set; }
+    public int MyPastVisits { get; set; }
+    
+    // Recent items
+    public IEnumerable<VisitBriefDto> RecentVisits { get; set; } = new List<VisitBriefDto>();
+    public IEnumerable<AnimalDashboardDto> RecentAnimals { get; set; } = new List<AnimalDashboardDto>();
+}
