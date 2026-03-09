2934f9ec22ff9263c7a8b131d600ff4967f5862a
psarnecki <piotr.sarnecki03@gmail.com>
2026-03-09 23:52:44 +0100

    Add automatic Client role assignment to new registered users

--- VetClinicManager/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ VetClinicManager/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -135,6 +135,9 @@ namespace VetClinicManager.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+                    
+                    await _userManager.AddToRoleAsync(user, "Client");
+                    _logger.LogInformation("Assigned 'Client' role to new user.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
